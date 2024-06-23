9996606db1377fe39d5ddbd8c061549510cfb3da
ErJun Wu <defyto13579@gmail.com>
2024-06-23 22:18:11 +0800

    響應式範例二

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <script setup>
     import {ref} from "vue";
-    let title = ref("網站標題")
+    let stat = ref({
+        title: "網站標題",
+        className: "title"
+    })
     const handler = function() {
-        console.log("click")
-        title.value = "新的網站標題"
+        stat.value.title = "新的網站標題";
+        stat.value.className = "title active";
     }
 </script>
 
 <template>
     <nav>基本的導覽列</nav>
     <main>
-        <h1>{{ title }}</h1>
+        <h1 :class="stat.className">{{ stat.title }}</h1>
         <button @click="handler">按鈕</button>
     </main>
 </template>
 
 <style scoped>
+    .active { color: red; }
     main { background: #999; }
 </style>
