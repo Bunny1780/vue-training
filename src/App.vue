65550631d4da63dcd25675778ab30983ad2b5718
ErJun Wu <defyto13579@gmail.com>
2024-09-30 14:35:31 +0800

    自定事件處理

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
     import Nav from "./Nav.vue";
     import Main from "./Main.vue";
+    import { ref } from "vue";
+
+    // 標準事件處理
+    let text = ref("測試文字");
+    let change = function() {
+        text.value = "修改文字"
+    }
 </script>
 
 <template>
     <Nav></Nav>
-    <Main></Main>
+    <!-- <Main></Main> -->
+    <Main @Myevent="change"></Main> <!-- 自訂事件處理 -->
+    <div>{{ text }}</div>
+
+    <!-- 標準事件處理 -->
+    <!-- <button @click="change">按鈕</button> -->
 </template>
 
 <style scoped>
